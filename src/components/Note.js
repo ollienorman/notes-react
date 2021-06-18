05e1a31d661b73b1786bdfe6bde3a16589b68f0c
Ollie <oliver.norman1991@gmail.com>
2021-06-18 16:47:10 +0100

    Add styled-components functionality, initial styling tested on Note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import styled from 'styled-components';
 
 // sort notes by ID (timestamp)
 
+const Wrapper = styled.div`
+  background-color: rgba(243, 225, 156, 0.5);
+`
+
 function Note(props) {
   return (
-    <div>
-      {props.title}
-      {props.body}
+    <Wrapper>
+      <h3> 
+        {props.title}
+      </h3>
+      <p>
+        {props.body}
+      </p>
       <button type="button" onClick={() => (props.deleteNote(props.id))}>&times; Delete Note</button>
-    </div>
+    </Wrapper>
   )
 }
 
