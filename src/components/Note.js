eaf3d502070dcfd23eb76a18cfb3e663c0b8feeb
Ollie <oliver.norman1991@gmail.com>
2021-06-21 17:43:14 +0100

    Add basic styling to DeleteButton within Note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,6 +6,21 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   background-color: rgba(243, 225, 156, 0.5);
+  min-height: 150px;
+  width: 300px;
+  margin-bottom: 20px;
+  padding: 5px;
+`;
+
+const DeleteButton = styled.button`
+  background-color: rgba(117, 117, 117, 1);
+  border: none;
+  color: white;
+  padding: 8px 22px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 `;
 
 function Note(props) {
@@ -18,9 +33,9 @@ function Note(props) {
     <Wrapper>
       <h3>{title}</h3>
       <p>{body}</p>
-      <button type="button" onClick={() => deleteNote(id)}>
+      <DeleteButton type="button" onClick={() => deleteNote(id)}>
         &times; Delete Note
-      </button>
+      </DeleteButton>
     </Wrapper>
   );
 }
